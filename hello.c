99593c0d001bcc929242fe311a04aae3ac3b7ee6
Walin Ahmed <walinasahmed@gmail.com>
2023-05-15 21:23:13 +0600

    2c

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    printf("hello, world");
+    printf("hello, world".);
+    printf("2 C or Not 2 C, that is the question.")
     return 0;
 }
